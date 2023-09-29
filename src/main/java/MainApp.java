183ef12ce6e11982bb2bbc9ec23d3e951b592b61
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-09-29 14:18:32 +0200

    Variables

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,23 @@
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+        System.out.println(number);
+
+        double doubleNumber = 10.5;
+        System.out.println(doubleNumber);
+
+        String someText = "text in java";
+        System.out.println(someText);
+
+        char someChar = 'd';
+        System.out.println(someChar);
+
+        boolean trueValue = true;
+        System.out.println(trueValue);
+
+        boolean falseValue = false;
+        System.out.println(falseValue);
+    }
+}
