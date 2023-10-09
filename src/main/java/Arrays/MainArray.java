96a36314ccb08ae7db4e2f893da0c3db74632918
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-10-09 15:16:30 +0200

    Added Arrays

---
+++ src/main/java/Arrays/MainArray.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class MainArray {
+
+    public static void main(String[] args) {
+
+        String names[] = new String[5];
+
+        names[0] = "Bartek";
+        System.out.println(names[0]);
+
+        names[1] = "Arek";
+        System.out.println(names[1]);
+
+        names[2] = "Monika";
+        System.out.println(names[2]);
+
+        names[3] = "Piotr";
+        System.out.println(names[3]);
+
+        names[4] = "Krzysztof";
+        System.out.println(names[4]);
+    }
+}
