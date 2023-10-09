8d0122fe8b9ba2527af5524dc80325498f0ba44f
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-10-09 16:20:07 +0200

    Added For and For each loop

--- src/main/java/Arrays/MainArray.java
+++ src/main/java/Arrays/MainArray.java
@@ -7,18 +7,20 @@ public class MainArray {
         String names[] = new String[5];
 
         names[0] = "Bartek";
-        System.out.println(names[0]);
-
         names[1] = "Arek";
-        System.out.println(names[1]);
-
         names[2] = "Monika";
-        System.out.println(names[2]);
-
         names[3] = "Piotr";
-        System.out.println(names[3]);
-
         names[4] = "Krzysztof";
-        System.out.println(names[4]);
+
+        for (int i = 0; i < names.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(names[i]);
+            }
+        }
+
+        for (String name : names
+        ) {
+            System.out.println(name);
+        }
     }
 }
