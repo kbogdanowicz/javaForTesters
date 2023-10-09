fec1262c3d368c20aa0baee40b808123d195fb3e
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-10-09 16:50:16 +0200

    Added Array, For and For each loop summary

--- src/main/java/Arrays/MainArray.java
+++ src/main/java/Arrays/MainArray.java
@@ -22,5 +22,21 @@ public class MainArray {
         ) {
             System.out.println(name);
         }
+
+        int[] newNumbers = {1, 2, 3, 4, 5};
+
+        String[] names1 = {"Bartek", "Marek", "Krzysztof", "Adam"};
+        System.out.println(names1.length);
+
+        for (String name1 : names1
+             ) {
+            System.out.println(name1.toUpperCase());
+        }
+
+        for (int i = 0; i < names1.length; i++) {
+            System.out.println(names1[i].toLowerCase());
+        }
+
+
     }
 }
