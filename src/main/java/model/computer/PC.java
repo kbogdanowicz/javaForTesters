dacbef88f7f9fa7a565c853a3aff716dd41c8ab3
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-10-12 12:51:06 +0200

    Added inheritance

---
+++ src/main/java/model/computer/PC.java
@@ -0,0 +1,62 @@
+package model.computer;
+
+public class PC {
+
+    private String name;
+    private String type;
+    private int hdd;
+    private int ram;
+    private boolean state;
+
+    public PC(String name, String type, int hdd, int ram) {
+        this.name = name;
+        this.type = type;
+        this.hdd = hdd;
+        this.ram = ram;
+        this.state = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public void switchOn() {
+        state = true;
+    }
+
+    public void switchOff() {
+        state = false;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+}
