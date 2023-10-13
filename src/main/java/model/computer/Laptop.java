646301829dbb4cf7093db3e7ff82cab0c080106a
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-10-13 11:54:52 +0200

    Added Parent class

--- src/main/java/model/computer/Laptop.java
+++ src/main/java/model/computer/Laptop.java
@@ -1,4 +1,8 @@
 package model.computer;
 
-public class Laptop {
+public class Laptop extends Computer{
+
+    public Laptop(String name, String type, int hdd, int ram) {
+        super(name, type, hdd, ram);
+    }
 }
--- src/main/java/model/computer/PC.java
+++ src/main/java/model/computer/PC.java
@@ -1,62 +1,9 @@
 package model.computer;
 
-public class PC {
-
-    private String name;
-    private String type;
-    private int hdd;
-    private int ram;
-    private boolean state;
+public class PC extends Computer{
 
     public PC(String name, String type, int hdd, int ram) {
-        this.name = name;
-        this.type = type;
-        this.hdd = hdd;
-        this.ram = ram;
-        this.state = false;
+        super(name, type, hdd, ram);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getHdd() {
-        return hdd;
-    }
-
-    public void setHdd(int hdd) {
-        this.hdd = hdd;
-    }
-
-    public int getRam() {
-        return ram;
-    }
-
-    public void setRam(int ram) {
-        this.ram = ram;
-    }
-
-    public void switchOn() {
-        state = true;
-    }
-
-    public void switchOff() {
-        state = false;
-    }
-
-    public boolean getState() {
-        return state;
-    }
 }
