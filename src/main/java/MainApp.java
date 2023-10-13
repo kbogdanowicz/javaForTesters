61e1948cd0740bc9171ba48fdb3ae42d138af7dc
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-10-13 15:22:12 +0200

    Added new field

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,17 +1,17 @@
 import model.computer.Laptop;
 import model.computer.PC;
-import utils.StringUtils;
-import utils.WeekUtils;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
         PC officeComputer = new PC("Office Computer", "HP", 500, 128);
-        Laptop gamingLaptorp = new Laptop("XGames", "HP GAMES", 500, 256);
+        Laptop gamingLaptop = new Laptop("XGames", "HP GAMES", 500, 256, 50);
 
-        officeComputer.switchOn();
-        gamingLaptorp.switchOn();
+        gamingLaptop.setBatteryLevel(0);
+
+        gamingLaptop.switchOn();
+        System.out.println(gamingLaptop.getState());
 
     }
 }
--- src/main/java/model/computer/Laptop.java
+++ src/main/java/model/computer/Laptop.java
@@ -2,13 +2,24 @@ package model.computer;
 
 public class Laptop extends Computer{
 
-    public Laptop(String name, String type, int hdd, int ram) {
+    private int batteryLevel;
+
+    public Laptop(String name, String type, int hdd, int ram, int batteryLevel) {
         super(name, type, hdd, ram);
+        this.batteryLevel = batteryLevel;
     }
 
     @Override
     public void switchOn() {
         System.out.println("Checking power level");
-        super.switchOn();
+        if (batteryLevel > 0) {
+            super.switchOn();
+        } else {
+            System.out.println("Battery level to low.");
+        }
+
+    }
+    public void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
     }
 }
