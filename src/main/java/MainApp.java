848282a557b257b90bd3a8ac1c479be90bdea473
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-10-18 17:10:59 +0200

    Added collections introduction

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,16 +1,47 @@
-import model.computer.*;
-
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-      Hdd hdd = new Hdd("Samsung", 500);
-      Computer mac = new Laptop("Mac", "PRO", hdd, new Ram("AAA", 128), 100);
+        List<String> names = new ArrayList<>();
+        List<String> names1 = new ArrayList<>();
 
-      System.out.println(mac.getHdd().getSize());
+        names.add("Bartek");
+        names.add("MArek");
+        names.add("Alicja");
+        names. add("Tomek");
+
+        names1.add("Bartek");
+        names1.add("Alicja");
+
+//        for (String name: names) {
+//            System.out.println(name);
+//        }
+
+//        names.remove(0);
+//        names.remove("Bartek");
+
+//        String name = names.get(0);
+//        System.out.println(name);
+
+//        boolean empty = names.isEmpty();
+//        boolean bartek = names.contains("Bartek");
+
+//        System.out.println(names.size());
+
+        names.addAll(names1);
+
+//        names.removeAll(names1);
+//        System.out.println(names);
+
+        Collections.sort(names);
+        Collections.reverse(names);
+
+        System.out.println(names);
 
     }
 }
