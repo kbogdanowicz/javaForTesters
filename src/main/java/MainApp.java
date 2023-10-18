32fefa011697186064f05543b7291bfe90600d61
Katarzyna Bogdanowicz <bogdanowicz.kaska@gmail.com>
2023-10-18 07:32:35 +0200

    Added wrapper types

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,12 +1,28 @@
 import model.computer.PC;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainApp {
 
     public static void main(String[] args) {
 
-        PC officeComputer1 = new PC("Office Computer 1", "HP", 500, 128);
+      Byte b;
+      Short s;
+      Integer i;
+      Long l;
+      Float fl;
+      Double d;
+      Character c;
+      Boolean bl;
 
-        officeComputer1.sayHello();
+      int number = 10;
+
+      Integer intNumber = 10;
+
+      int newNumber = intNumber.intValue();
+
+        List<Integer> numbers = new ArrayList<>();
 
     }
 }
